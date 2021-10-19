5e4baf0b9b0cf74bb41204a110c7e68b8a9ff57c
kmstan <92769763+kmstan@users.noreply.github.com>
2021-10-19 15:48:55 +0800

    Add files via upload

---
+++ 04_Vivado/yolo_v2.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2021.1 (win64) Build 3247384 Thu Jun 10 19:36:33 MDT 2021
+// Date        : Fri Oct 15 13:29:43 2021
+// Host        : LAPTOP-SD1336R4 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/STPH/yolo_v2/yolo_v2.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0_stub.v
+// Design      : design_1_clk_wiz_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module design_1_clk_wiz_0_0(clk_out1, resetn, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,resetn,locked,clk_in1" */;
+  output clk_out1;
+  input resetn;
+  output locked;
+  input clk_in1;
+endmodule
